193b93bb6e2fa9f7b3759ee83e7ed1ffc7ce08a5
Kendra Chateau <kendra@super.gd>
2021-06-23 15:59:25 -0700

    Get game stats
    Added a call to the new steam wrapper gem to get the users owned games,
    currently just displaying them in an ugly table until I figure out what
    Im using for the frontend stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
+require 'steam/wrapper/user'
+
 class HomeController < ApplicationController
   def index
+    @owned_games = owned_games
+  end
+
+  private
+
+  def client
+    @client ||= Steam::Wrapper::User.new(ENV['STEAM_API_KEY'])
+  end
+
+  def owned_games
+    client.get_owned_games(current_user.steam_id)
   end
 end
