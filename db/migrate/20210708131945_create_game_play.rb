f4a2f614ef585247aa1b0118e26e277301e36aab
Kendra Chateau <kendra@super.gd>
2021-07-13 10:51:20 -0700

    Create game play migration
    Created a migration for a game play which links a steam user to a game
    and offers the different fields for playtime forever and on different
    platforms

---
+++ db/migrate/20210708131945_create_game_play.rb
@@ -0,0 +1,13 @@
+class CreateGamePlay < ActiveRecord::Migration[5.2]
+  def change
+    create_table :game_plays do |t|
+      t.references :steam_user, index: true
+      t.references :game, index: true
+      t.ingeter :playtime_forever
+      t.integer :playtime_2weeks
+      t.integer :playtime_windows_forever
+      t.integer :playtime_mac_forever
+      t.integer :playtime_linux_forever
+    end
+  end
+end
