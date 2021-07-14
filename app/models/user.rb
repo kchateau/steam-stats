5af42c8e1ef34a37131e7725eb6f5aca1e5c04b7
Kendra Chateau <kendra@super.gd>
2021-07-13 19:44:02 -0700

    Create SteamUser on registration
    This will now create a SteamUser when the user signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_create :create_steam_user
+
+  private 
+
+  def create_steam_user
+    SteamUser.create(steam_id: self.steam_id)
+  end
 end
